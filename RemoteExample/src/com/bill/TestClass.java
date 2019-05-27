158fbc36f1c2c11cb57272a9b0a881b6d2bb623a
billg <billg@DESKTOP-QLIVIDI>
2019-05-27 14:51:51 -0500

    me

--- RemoteExample/src/com/bill/TestClass.java
+++ RemoteExample/src/com/bill/TestClass.java
@@ -1,5 +1,25 @@
 package com.bill;
 
-public class TestClass {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+public class TestClass {
+	
+	public static void main(String[] args) {
+	    String myDate = "05/1/2018";
+	    String year = myDate.substring(myDate.length()-4, myDate.length());
+	      int slsh = myDate.indexOf("/");
+	    String day = myDate.substring(0,slsh);
+	    String month = myDate.substring(slsh+1, myDate.length()-5);
+	    
+	    String newDate = year + "-" + month  + "-" +day;
+	
+		SimpleDateFormat sfd = new SimpleDateFormat("hh:mm:ss");
+		Calendar cal = Calendar.getInstance();
+	    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+	    String mytime =  sdf.format(cal.getTime()) ;
+		String newDteTime = newDate + " " + mytime;
+		
+		System.out.println(newDteTime);
+	}
 }
